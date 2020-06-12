ef50fed181b79b2523bcdff74be07a4bf92babe4
guygastineau <strings.stringsandstrings@gmail.com>
2020-06-12 00:16:24 -0400

    bumped the nom version to `6.0.0-alpha` and now the Parser trait is found

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{
+        alpha0,
+        alpha1,
+        char,
+        digit1,
+        multispace0,
+        multispace1,
+        one_of,
+    },
+    combinator::{ cut, map, map_res, opt },
+    error::{ context, VerboseError },
+    multi::many0,
+    sequence::{ delimited, preceded, terminated, tuple },
+    IResult,
+    Parser,
+};
+
 #[cfg(test)]
 mod tests {
     #[test]
